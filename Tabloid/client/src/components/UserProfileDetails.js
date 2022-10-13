916db2f16858a9147a9e8e1bbe592b34ed754a6f
Haley Hickey <hchickey96@gmail.com>
2022-10-13 09:44:04 -0500

    view all UserDetails

--- Tabloid/client/src/components/UserProfileDetails.js
+++ Tabloid/client/src/components/UserProfileDetails.js
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
 import { getUserProfileById } from "../modules/userProfileManager";
-import { ProfileDetails } from "./ProfileDetails";
+import { Card, CardBody } from "reactstrap"
+
 
 
 export const UserProfileDetails = () => {
     const { firebaseUserId } = useParams();
-    const [details, setDetails] = useState({});
+    const [detail, setDetail] = useState({});
 
     const getDetails = (id) => {
         getUserProfileById(id).then((userProfile) => {
-            setDetails(userProfile);
+            setDetail(userProfile);
         });
     };
 
@@ -19,10 +20,22 @@ export const UserProfileDetails = () => {
     }, []);
 
     return (
-        <section>
-            {details.map((profileDetails) => (
-                <ProfileDetails key={profileDetails.id} profileDetails={profileDetails} />
-            ))}
-        </section>
-    );
+        <Card className="m-4">
+            <h1>User Profile Details</h1>
+            <CardBody>
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <p>
+                            <img src={detail.imageLocation} />
+                        </p>
+                        <p>Name: {detail.fullName}</p>
+                        <p>Display Name: {detail.displayName}</p>
+                        <p>Email: {detail.email}</p>
+                        <p>Date: {detail.createDateTime}</p>
+                        <p>UserType: {detail.userType?.name}</p>
+                    </div>
+                </div>
+            </CardBody>
+        </Card>
+    )
 }
